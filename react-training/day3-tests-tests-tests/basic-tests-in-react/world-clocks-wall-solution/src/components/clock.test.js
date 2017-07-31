d0b77b2cd49555657c73e429664cf3472408b8d6
Nir Moav <nirm@wix.com>
2017-07-31 14:50:18 +0300

    setting up a simple test for a clock label to equal "Tel Aviv"

--- react-training/day3-tests-tests-tests/basic-tests-in-react/world-clocks-wall-solution/src/components/clock.test.js
+++ react-training/day3-tests-tests-tests/basic-tests-in-react/world-clocks-wall-solution/src/components/clock.test.js
@@ -3,18 +3,13 @@ import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
 import {expect} from 'chai';
 
-import Location from '../location';
-import Clock from './clock';
+// import your components here
 
 describe('Clock Component', () => {
-  it('should render the passed Label of the Clock', () => {
-    const localTime = 122333444455555;
-    const detachedDiv = document.createElement('div');
-    const clockComponent = ReactDOM.render(<Clock timeInMs={localTime} location={Location('Tel Aviv', 0)} />, detachedDiv);
-
-    const labelElement = ReactTestUtils.findRenderedDOMComponentWithClass(clockComponent, 'clock-label');
-
-    expect(labelElement.textContent).to.eq('Tel Aviv');
+  it('should render the passed Label of the Clock equals to Tel Aviv', () => {
+    let clockLabel = '';
+    // create a clock, and set its label to Tel Aviv
+    expect(clockLabel).to.eq('Tel Aviv');
   });
 });
 
